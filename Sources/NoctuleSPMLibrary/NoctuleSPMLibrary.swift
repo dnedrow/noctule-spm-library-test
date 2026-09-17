affeb65cfd93349a453a29f0f61869bd69b5d248
David Nedrow <dnedrow@me.com>
2026-09-17 14:12:58 -0400

    chore: Add SwiftUI view and update .gitignore

--- Sources/NoctuleSPMLibrary/NoctuleSPMLibrary.swift
+++ Sources/NoctuleSPMLibrary/NoctuleSPMLibrary.swift
@@ -1,5 +1,25 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-public func hello() {
-    print("Hello, world!")
+//
+//  SwiftUIView.swift
+//  NoctuleSPMLibrary
+//
+//  Created by David Nedrow on 9/17/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    @State private var hapticTrigger: Int = 0
+    var body: some View {
+        Text("Hello, World!")
+
+        Button {
+            //
+        } label: {
+        }
+        .sensoryFeedback(.impact(weight: .medium), trigger: hapticTrigger)
+    }
+}
+
+#Preview {
+    SwiftUIView()
 }
